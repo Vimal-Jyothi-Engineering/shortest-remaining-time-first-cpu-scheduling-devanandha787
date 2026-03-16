05b8c22637dbc079c1fc13cd533a548c773112a3
devanandha787 <vml24ad047@gmail.com>
2026-03-16 11:41:09 +0530

    priority.c

--- priority.c
+++ priority.c
@@ -1,76 +1,92 @@
-// srtf.c
-// Shortest Remaining Time First (Preemptive SJF)
-
 #include <stdio.h>
-#include <limits.h>
+#include <ctype.h>
+
+struct process {
+    char pid[10];
+    int at, bt, rt;
+    int ct, wt, tat;
+};
 
 int main() {
-    int n;
 
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
+    struct process p[20];
+    int n = 0;
 
-    int pid[20], at[20], bt[20], rt[20];
-    int ct[20], tat[20], wt[20];
+    char first[10];
 
-    for (int i = 0; i < n; i++) {
-        pid[i] = i + 1;
+    scanf("%s", first);
 
-        printf("Enter Arrival Time for P%d: ", pid[i]);
-        scanf("%d", &at[i]);
+    if(isdigit(first[0])) {
+        n = first[0] - '0';
 
-        printf("Enter Burst Time for P%d: ", pid[i]);
-        scanf("%d", &bt[i]);
+        for(int i = 0; i < n; i++) {
+            scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
+            p[i].rt = p[i].bt;
+        }
+    } 
+    else {
+        sscanf(first, "%s", p[0].pid);
+        scanf("%d %d", &p[0].at, &p[0].bt);
+        p[0].rt = p[0].bt;
+        n = 1;
 
-        rt[i] = bt[i];   // remaining time
+        while(scanf("%s %d %d", p[n].pid, &p[n].at, &p[n].bt) == 3) {
+            p[n].rt = p[n].bt;
+            n++;
+        }
     }
 
-    int completed = 0, time = 0;
-    int min_rt, shortest = -1;
+    int completed = 0, time = 0, min_index;
+    int min_rt;
 
-    while (completed < n) {
+    while(completed < n) {
 
-        min_rt = INT_MAX;
-        shortest = -1;
+        min_rt = 9999;
+        min_index = -1;
 
-        for (int i = 0; i < n; i++) {
-            if (at[i] <= time && rt[i] > 0 && rt[i] < min_rt) {
-                min_rt = rt[i];
-                shortest = i;
+        for(int i = 0; i < n; i++) {
+            if(p[i].at <= time && p[i].rt > 0 && p[i].rt < min_rt) {
+                min_rt = p[i].rt;
+                min_index = i;
             }
         }
 
-        if (shortest == -1) {
+        if(min_index == -1) {
             time++;
             continue;
         }
 
-        rt[shortest]--;
+        p[min_index].rt--;
         time++;
 
-        if (rt[shortest] == 0) {
+        if(p[min_index].rt == 0) {
             completed++;
 
-            ct[shortest] = time;
-            tat[shortest] = ct[shortest] - at[shortest];
-            wt[shortest] = tat[shortest] - bt[shortest];
+            p[min_index].ct = time;
+            p[min_index].tat = p[min_index].ct - p[min_index].at;
+            p[min_index].wt = p[min_index].tat - p[min_index].bt;
+
+            if(p[min_index].wt < 0)
+                p[min_index].wt = 0;
         }
     }
 
-    float total_wt = 0, total_tat = 0;
+    float avg_wt = 0, avg_tat = 0;
 
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
-
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
-
-        total_wt += wt[i];
-        total_tat += tat[i];
+    printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].wt);
+        avg_wt += p[i].wt;
     }
 
-    printf("\nAverage Turnaround Time = %.2f", total_tat / n);
-    printf("\nAverage Waiting Time = %.2f\n", total_wt / n);
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s %d\n", p[i].pid, p[i].tat);
+        avg_tat += p[i].tat;
+    }
+
+    printf("Average Waiting Time: %.1f\n", avg_wt/n);
+    printf("Average Turnaround Time: %.1f\n", avg_tat/n);
 
     return 0;
 }
