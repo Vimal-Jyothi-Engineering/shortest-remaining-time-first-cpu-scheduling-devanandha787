ebc8489b04a41ef4e0178e73ec1bc84ddd307e56
devanandha787 <vml24ad047@gmail.com>
2026-03-17 10:48:51 +0530

    priority.c

--- priority.c
+++ priority.c
@@ -1,74 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Process {
-    char pid[10];
-    int at, bt;
-    int rt;
-    int ct, tat, wt;
-};
+#define MAX 100
 
 int main() {
-
     int n;
+    char pid[MAX][10];
+    int at[MAX], bt[MAX], rt[MAX];
+    int ct[MAX], tat[MAX], wt[MAX];
+    
+    printf(""); // No extra prompt (important for autograder)
     scanf("%d", &n);
 
-    struct Process p[n];
-
-    for(int i = 0; i < n; i++) {
-        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
-        p[i].rt = p[i].bt;
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d %d", pid[i], &at[i], &bt[i]);
+        rt[i] = bt[i]; // Remaining time initialized
     }
 
-    int completed = 0;
-    int time = 0;
-    int min_index = -1;
+    int completed = 0, time = 0;
+    int min_index;
     int min_rt;
+    int found;
 
-    while(completed < n) {
+    while (completed < n) {
+        min_rt = 1e9;
+        found = 0;
 
-        min_rt = 9999;
-        min_index = -1;
-
-        for(int i = 0; i < n; i++) {
-            if(p[i].at <= time && p[i].rt > 0 && p[i].rt < min_rt) {
-                min_rt = p[i].rt;
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && rt[i] > 0 && rt[i] < min_rt) {
+                min_rt = rt[i];
                 min_index = i;
+                found = 1;
             }
         }
 
-        if(min_index == -1) {
-            time++;
+        if (!found) {
+            time++; // CPU idle
             continue;
         }
 
-        p[min_index].rt--;
+        // Execute for 1 unit
+        rt[min_index]--;
         time++;
 
-        if(p[min_index].rt == 0) {
+        // If process finishes
+        if (rt[min_index] == 0) {
             completed++;
-
-            p[min_index].ct = time;
-            p[min_index].tat = p[min_index].ct - p[min_index].at;
-            p[min_index].wt = p[min_index].tat - p[min_index].bt;
+            ct[min_index] = time;
+            tat[min_index] = ct[min_index] - at[min_index];
+            wt[min_index] = tat[min_index] - bt[min_index];
         }
     }
 
-    float total_wt = 0;
-    float total_tat = 0;
+    float total_wt = 0, total_tat = 0;
 
+    // Output: Waiting Time
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++) {
-        printf("%s %d\n", p[i].pid, p[i].wt);
-        total_wt += p[i].wt;
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", pid[i], wt[i]);
+        total_wt += wt[i];
     }
 
+    // Output: Turnaround Time
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++) {
-        printf("%s %d\n", p[i].pid, p[i].tat);
-        total_tat += p[i].tat;
+    for (int i = 0; i < n; i++) {
+        printf("%s %d\n", pid[i], tat[i]);
+        total_tat += tat[i];
     }
 
+    // Averages
     printf("Average Waiting Time: %.1f\n", total_wt / n);
     printf("Average Turnaround Time: %.1f\n", total_tat / n);
 
